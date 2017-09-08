af201d2939265eb6fedb513a9ce4f9fdccd56315
manoj@conqsys.com <manoj@conqsys.com>
2017-09-08 15:39:04 +0530

    gun-gen select image

--- src/app/pages/gun-gen/gun-gen.component.ts
+++ src/app/pages/gun-gen/gun-gen.component.ts
@@ -5,7 +5,7 @@ import { HeightMapSocketService } from './HeightMapSocketService';
 import { GlobalRef } from '../../global-ref';
 import * as firebase from 'firebase';
 import { GunSocketService } from './gun-socket.service';
-import { inputVal,outputVal } from './mock-data';  //dummy data
+import { inputVal, outputVal } from './mock-data';  //dummy data
 declare var $: any;
 declare var ValidateInputsThenApply: any;
 
@@ -15,8 +15,7 @@ declare var ValidateInputsThenApply: any;
   templateUrl: './gun-gen.component.html',
   styleUrls: ['./gun-gen.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  providers:[ GunSocketService ],
-  
+  providers: [GunSocketService]
 })
 export class GunGenComponent implements AfterViewInit {
 
@@ -27,74 +26,74 @@ export class GunGenComponent implements AfterViewInit {
   guns = [];
   selectedImgs = [];
   userTerrains: any;
-  gunFiles=[];
+  gunFiles = [];
   /* Received Data after clicking on button Upload */
   receivedData: any[] = [];
   showDeleteSelected = false;
   @Input() generationType: string;
 
-  toastr:any;
+  toastr: any;
   constructor(public gunGenService: GunInterpService,
-              private http: Http,
-              private socket: HeightMapSocketService,
-              private global: GlobalRef,
-              private gSocket:GunSocketService
-            ) {
-              const wnd = this.global.nativeGlobal;
-              this.toastr = wnd.toastr;
+    private http: Http,
+    private socket: HeightMapSocketService,
+    private global: GlobalRef,
+    private gSocket: GunSocketService
+  ) {
+    const wnd = this.global.nativeGlobal;
+    this.toastr = wnd.toastr;
 
-              this.gSocket.on("connection",()=>{
-                  console.log("Connected");
-                  this.showToast("Socket connected");
-              });
+    this.gSocket.on("connection", () => {
+      console.log("Connected");
+      this.showToast("Socket connected");
+    });
 
-              this.gSocket.on("files",(data)=>{
+    this.gSocket.on("files", (data) => {
 
-                console.log("Files received");
-                console.log(data);
-                
-                this.showToast("Files received");
-                this.gunFiles=data["files"];
-                  this.receivedData.push(this.gunFiles);
-              });
+      console.log("Files received");
+      console.log(data);
 
-              this.gSocket.on("errorInfo",(data)=>{
+      this.showToast("Files received");
+      this.gunFiles = data["files"];
+      this.receivedData.push(this.gunFiles);
+    });
 
-                console.log(data);
-                
-                this.showToast("Info: "+data["msg"]);
-                
-              });
-              //for gungenImages
-              this.gSocket.on("imageFile",(d)=>{
-                console.log("ImageFile received");
-                console.log(d);
-                this.gunFiles=this.gunFiles.map((x)=>{
-                  if(x.id==d.file.id)
-                   {return d.file} else{} return x    });
+    this.gSocket.on("errorInfo", (data) => {
 
-                  this.receivedData=this.receivedData.map((x)=>{ 
-                    if((x[0]).id!=(this.gunFiles[0]).id)
-                      return x
-                    else
-                      return this.gunFiles
-                     });
-              });
+      console.log(data);
 
-                this.gunGenService.isReady().subscribe(user=>{
+      this.showToast("Info: " + data["msg"]);
 
-                  if(user){
-                 //this.getGuns(this.activeLink);
-                 console.log("calling nectTer"+user +"received");
-                 this.nextTerGan();
-                }
-                })
+    });
+    //for gungenImages
+    this.gSocket.on("imageFile", (d) => {
+      console.log("ImageFile received");
+      console.log(d);
+      this.gunFiles = this.gunFiles.map((x) => {
+        if (x.id == d.file.id)
+        { return d.file } else { } return x
+      });
 
-    }
+      this.receivedData = this.receivedData.map((x) => {
+        if ((x[0]).id != (this.gunFiles[0]).id)
+          return x
+        else
+          return this.gunFiles
+      });
+    });
+
+    this.gunGenService.isReady().subscribe(user => {
+
+      if (user) {
+        //this.getGuns(this.activeLink);
+        console.log("calling nectTer" + user + "received");
+        this.nextTerGan();
+      }
+    })
+
+  }
 
 
-  showToast(msg){
-
+  showToast(msg) {
     this.toastr.info(msg);
   }
   ngAfterViewInit() {
@@ -104,29 +103,33 @@ export class GunGenComponent implements AfterViewInit {
       let item;
       const imgs = document.getElementById('accordion').getElementsByTagName('img');
       const imgList = [];
-      for ( let i = 0; i < imgs.length; i++ ) {
-        if ( imgs[i].src === msg.path ) {
+      for (let i = 0; i < imgs.length; i++) {
+        if (imgs[i].src === msg.path) {
           item = imgs[i];
           break;
         }
       }
-      if ( item ) {
+      if (item) {
         item.src = msg.path;
       }
     });
     this.nextTerGan();
   }
   getGuns(type: string) {
-    console.log("GFetting guns "+type);
+    console.log("GFetting guns " + type);
     this.gunGenService.getGuns(type)
       .then(data => {
         console.log('DATAAAAA');
         console.log(data);
-        this.guns = data;
+        this.guns=[];
+        data.map(res => {
+          this.guns.splice(this.guns.length,-1,{isSelect:false, url:res})
+        })
+        // this.guns = data;
       })
       .catch(error => console.log(error));
-   
   }
+
   selectGun(type: string) {
     this.activeLink = type;
     this.getGuns(type);
@@ -134,6 +137,7 @@ export class GunGenComponent implements AfterViewInit {
   imageLoaded(event) {
     event.target.style.visibility = 'visible';
   }
+
 
   errorImage(event) {
     event.target.style.visibility = 'hidden';
@@ -152,22 +156,22 @@ export class GunGenComponent implements AfterViewInit {
             .ref(`gunImages/${item.type}/`)
             .child(`${item.name}`)
             .getDownloadURL()
-            .then(data => {item["imgsrc"]=data;
-            item["active"]=false
-                //console.log(item);
-                return item;
-          }).catch(e=>console.log(e))
-        );
+            .then(data => {
+              item["imgsrc"] = data;
+              //console.log(item);
+              return item;
+            }).catch(e => console.log(e))
+          );
         });
-         Promise.all(itemsArr).then((d)=>{this.userTerrains=d;console.log(d)});
+        Promise.all(itemsArr).then((d) => { this.userTerrains = d; console.log(d) });
         console.log(this.userTerrains);
       });
     //this.isGenerate = !this.isGenerate;
-      console.log(this.isGenerate);
+    console.log(this.isGenerate);
   }
 
-  showHideSidePane(){
-    this.isGenerate=!this.isGenerate;
+  showHideSidePane() {
+    this.isGenerate = !this.isGenerate;
     console.log("Switching Side panes");
 
   }
@@ -177,10 +181,15 @@ export class GunGenComponent implements AfterViewInit {
 
   }
 
+  changeBorder = function (index) {
+    this.guns[index].isSelect = !this.guns[index].isSelect;
+    // this.selected = index;
+  }
+
   deleteFromLibrary(terrain) {
     console.log(terrain);
 
-    this.userTerrains=this.userTerrains.filter((x)=>x.$key!=terrain.$key);
+    this.userTerrains = this.userTerrains.filter((x) => x.$key != terrain.$key);
     this.gunGenService.removeGunFromLibrary(terrain.$key);
     /*this.gunGenService.getGunsFromLibrary('mountains')
       .subscribe(items => {
@@ -245,10 +254,10 @@ export class GunGenComponent implements AfterViewInit {
   clearCheckImages() {
 
     const images = document.getElementsByClassName('item');
-    for ( let i = 0; i < images.length; i++ ) {
-      if ( images[i].getElementsByTagName('input')[0] &&
+    for (let i = 0; i < images.length; i++) {
+      if (images[i].getElementsByTagName('input')[0] &&
         images[i].getElementsByTagName('input')[0].type === 'checkbox' &&
-        images[i].getElementsByTagName('input')[0].checked ) {
+        images[i].getElementsByTagName('input')[0].checked) {
         images[i].getElementsByTagName('input')[0].checked = false;
         const test = images[i].getElementsByClassName('fa-check-circle-o') as HTMLCollectionOf<HTMLElement>;
         test[0].style.display = test[0].style.display === 'none' ? '' : 'none';
@@ -262,31 +271,30 @@ export class GunGenComponent implements AfterViewInit {
   uploadImages(p_cross, minCount) {
     const wnd = this.global.nativeGlobal;
     const toastr = wnd.toastr;
-    
 
-    if ( this.selectedImgs.length < minCount ) {
-        toastr.error('Select only 2 Imagemaps for hybrid generate');
-        return;
-        
-      }
-      if(this.selectedImgs.length >2)
-        {
-          toastr.error('Select only 2 Imagemaps for hybrid generate');
-          return;
-      
-        }
-      
+
+    if (this.selectedImgs.length < minCount) {
+      toastr.error('Select only 2 Imagemaps for hybrid generate');
+      return;
+
+    }
+    if (this.selectedImgs.length > 2) {
+      toastr.error('Select only 2 Imagemaps for hybrid generate');
+      return;
+
+    }
+
 
     this.clearCheckImages();
 
     this.toastr.info("Downloading guns Json")
-    this.gunGenService.getGunJson(this.selectedImgs[0],this.selectedImgs[1]).then(val=>{
+    this.gunGenService.getGunJson(this.selectedImgs[0], this.selectedImgs[1]).then(val => {
 
       console.log("JSON VALUES DOWNLOAED");
-       this.toastr.info("Generating gun Models");
+      this.toastr.info("Generating gun Models");
       console.log(val);
-      this.generateGun(val[0],val[1]);
-    }).catch((err)=>{
+      this.generateGun(val[0], val[1]);
+    }).catch((err) => {
       console.log("Failed to download JSONS");
       this.toastr.error("Failed to download guns Json.")
 
@@ -298,7 +306,7 @@ export class GunGenComponent implements AfterViewInit {
     const images = document.getElementsByClassName('item');
     //for (let i = 0; i < images.length; i++) {
     //if ( images[i].getElementsByTagName('input')[0] && images[i].getElementsByTagName('input')[0].checked) {
-    if ( event.currentTarget.getElementsByTagName('input')[0] && event.currentTarget.getElementsByTagName('input')[0].checked ) {
+    if (event.currentTarget.getElementsByTagName('input')[0] && event.currentTarget.getElementsByTagName('input')[0].checked) {
       const test = event.currentTarget.getElementsByClassName('fa-check-circle-o');
       test[0].style.display = test[0].style.display === 'none' ? '' : 'none';
       event.currentTarget.getElementsByTagName('input')[0].checked = true;
@@ -306,63 +314,17 @@ export class GunGenComponent implements AfterViewInit {
       event.currentTarget.classList.toggle('active-img');
     }
 
-    event.currentTarget.classList.toggle('active-img');
-    
-    //if ( images[i].getElementsByTagName('input')[0] && images[i].getElementsByTagName('input')[0].checked) {
-    //event.currentTarget.classList.toggle('active-img');
-    tera["active"]=!tera["active"];
-   
-    
-
-    
     const images2 = document.getElementById('gen2-images').getElementsByClassName('item');
     const selectedCount = 0;
     this.showDeleteSelected = false;
-    let gunExist=this.selectedImgs.find(x=>x.$key==tera.$key);
-    this.userTerrains=this.userTerrains.map(x=>{ 
-        if(x.$key==tera.$key)
-          return tera 
-        else
-        return x 
-  })
-    console.log(gunExist);
-    if(gunExist)
-      this.selectedImgs=this.selectedImgs.filter(x=>x.$key!=tera.$key);
-    else
-      this.selectedImgs.push(tera)
-    console.log(this.selectedImgs);
-     for ( let i = 0; i < images2.length; i++ ) {
-      if ( images2[i].getElementsByTagName('input')[0] &&
+    this.selectedImgs = []
+    for (let i = 0; i < images2.length; i++) {
+      if (images2[i].getElementsByTagName('input')[0] &&
         images2[i].getElementsByTagName('input')[0].type === 'checkbox' &&
-        images2[i].getElementsByTagName('input')[0].checked ) {
+        images2[i].getElementsByTagName('input')[0].checked) {
         this.showDeleteSelected = false;
-    //    this.selectedImgs.push(this.userTerrains[i]);
+        this.selectedImgs.push(tera);
       }
-    }
-    console.log(this.selectedImgs);
-//    event.stopPropagation()
-  }
-
-  stopEvent(event)
-{  event.currentTarget["checked"]=false;
-event.stopPropagation();
-
-}
-  clickTest(event,ter){
-
-    console.log("Tile Clicked");
-
-    console.log(event);
-    const images = document.getElementsByClassName('item');
-    //for (let i = 0; i < images.length; i++) {
-    //if ( images[i].getElementsByTagName('input')[0] && images[i].getElementsByTagName('input')[0].checked) {
-    if ( event.currentTarget.getElementsByTagName('input')[0] && event.currentTarget.getElementsByTagName('input')[0].checked ) {
-      const test = event.currentTarget.getElementsByClassName('fa-check-circle-o');
-      test[0].style.display = test[0].style.display === 'none' ? '' : 'none';
-      event.currentTarget.getElementsByTagName('input')[0].checked = true;
-      //images[i].classList.toggle('active-img');
-      event.currentTarget.classList.toggle('active-img');
-      
     }
 
   }
@@ -370,9 +332,9 @@ event.stopPropagation();
   deleteSelected() {
     this.gunGenService.getGunsFromLibrary('mountains')
       .subscribe(items => {
-        for ( const item of items ) {
-          for ( const selected of this.selectedImgs ) {
-            if ( (item as any ).type === 'mountains' && (item as any).name === selected.match(/%2F(.+)\?/)[1] ) {
+        for (const item of items) {
+          for (const selected of this.selectedImgs) {
+            if ((item as any).type === 'mountains' && (item as any).name === selected.match(/%2F(.+)\?/)[1]) {
               this.gunGenService.removeTerrainsFromLibray((item as any).$key);
             }
           }
@@ -382,9 +344,9 @@ event.stopPropagation();
   }
 
 
-  generateGun(inval,outval){
-          console.log("sending files");
-          this.gSocket.emit("upload",{inputValues:inval,outputValues:outval});
+  generateGun(inval, outval) {
+    console.log("sending files");
+    this.gSocket.emit("upload", { inputValues: inval, outputValues: outval });
 
   }
 
